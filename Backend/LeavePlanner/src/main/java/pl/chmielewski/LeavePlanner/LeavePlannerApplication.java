be3d615e435ca9afc27097ad3a258de2ac3c5413
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-06-18 12:38:24 +0200

    Init backend

---
+++ Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/LeavePlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.chmielewski.LeavePlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavePlannerApplication.class, args);
+	}
+
+}
---
+++ Backend/LeavePlanner/src/test/java/pl/chmielewski/LeavePlanner/LeavePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.chmielewski.LeavePlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
