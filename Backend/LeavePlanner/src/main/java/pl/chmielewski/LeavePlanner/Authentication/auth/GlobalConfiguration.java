d73ec09b8ed49d86780fbc2ce9c65d7a728b5b3a
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-06-18 13:14:42 +0200

    Konfiguracje aplikacji, bazy danych, passwordEncoder

---
+++ Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/auth/GlobalConfiguration.java
@@ -0,0 +1,14 @@
+package pl.chmielewski.LeavePlanner.Authentication.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class GlobalConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
