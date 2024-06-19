deb564aaa9b4b4beabfe3d5dfae872636fc57606
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-06-19 14:16:34 +0200

    Zmiana nazwy klasy rozszerzanej

--- Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/api/response/UserDeletedResponse.java
+++ Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/api/response/UserDeletedResponse.java
@@ -1,8 +1,8 @@
 package pl.chmielewski.LeavePlanner.Authentication.api.response;
 
-import pl.chmielewski.LeavePlanner.Authentication.api.AbstractResponse;
+import pl.chmielewski.LeavePlanner.Authentication.api.AbstractApiResponse;
 
-public class UserDeletedResponse extends AbstractResponse {
+public class UserDeletedResponse extends AbstractApiResponse {
 
     public UserDeletedResponse(Long id) {
         super("User deleted with id: " + id, 201);
--- Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/api/response/UserUpdatedResponse.java
+++ Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/api/response/UserUpdatedResponse.java
@@ -1,9 +1,9 @@
 package pl.chmielewski.LeavePlanner.Authentication.api.response;
 
 
-import pl.chmielewski.LeavePlanner.Authentication.api.AbstractResponse;
+import pl.chmielewski.LeavePlanner.Authentication.api.AbstractApiResponse;
 
-public class UserUpdatedResponse extends AbstractResponse {
+public class UserUpdatedResponse extends AbstractApiResponse {
     public UserUpdatedResponse(Long id) {
         super("User was updated with id: " + id, 201);
     }
