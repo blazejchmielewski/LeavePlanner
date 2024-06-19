3dbc9aa825d8bacf3e78cebb1f4122b96b528050
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-06-19 10:58:58 +0200

    Zmieniam lokaliazacje

--- Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/auth/config/CorsConfiguration.java
+++ Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/auth/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package pl.chmielewski.LeavePlanner.Authentication.auth;
+package pl.chmielewski.LeavePlanner.Authentication.auth.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/auth/config/SecurityConfiguration.java
+++ Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/auth/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package pl.chmielewski.LeavePlanner.Authentication.auth;
+package pl.chmielewski.LeavePlanner.Authentication.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
