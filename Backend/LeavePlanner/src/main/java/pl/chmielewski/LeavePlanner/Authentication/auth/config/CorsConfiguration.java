8089a306bd3ac84b661428d4a2622bddacab1c89
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2024-06-19 14:18:46 +0200

    Zmiana pod angulara

--- Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/auth/config/CorsConfiguration.java
+++ Backend/LeavePlanner/src/main/java/pl/chmielewski/LeavePlanner/Authentication/auth/config/CorsConfiguration.java
@@ -9,8 +9,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
