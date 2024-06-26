e53f7c2ad274b249d9f2eb104aa0e2995f12cb1c
blazej.chmielewski <blazej.chmielewski@cfsa.pl>
2024-06-26 13:22:39 +0200

    Pobieranie UID

--- Frontend/CFSA-Leave-Planner/src/app/modules/auth/components/account-activation/account-activation.component.ts
+++ Frontend/CFSA-Leave-Planner/src/app/modules/auth/components/account-activation/account-activation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { AuthService } from 'src/app/modules/core/services/auth.service';
 
 @Component({
   selector: 'app-account-activation',
@@ -8,13 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AccountActivationComponent implements OnInit{
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private authService: AuthService){}
   
   ngOnInit(): void {
     this.route.paramMap.subscribe({
       next: (param) => {
         console.log(param.get('uid'));
       }
-    })
+    });
+
+    this.route.paramMap.pipe(
+      switchMap(params => this.authService.activateAccount(params.get('uid') as string))
+    )
   }
+
+  
 }
